0c39eadb5427a621dc71f420d8e09b34b24ac031
Natan Maia <natanmaiaplays@outlook.com>
2025-01-06 18:03:41 -0300

    primeiro upload e PRS

---
+++ fetch_perceval_signalandroid.py
@@ -0,0 +1,51 @@
+from perceval.backends.core.github import GitHub as Github  # pip install perceval
+
+from dotenv import load_dotenv # pip install python-dotenv
+import json
+import os
+
+'''
+Como usar o token:
+criar um arquivo config.env, inserir
+GITHUB_TOKEN=<personal_access_token>
+'''
+
+load_dotenv('config.env')
+# Retrieve the token from the environment
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+if not GITHUB_TOKEN:
+    raise ValueError("GitHub token not found. Please set it in config.env")
+
+
+# Define the repository details
+OWNER = "signalapp"
+REPO = "Signal-Android"
+print("token: ", GITHUB_TOKEN)
+# exit()
+
+# Initialize the Github backend
+# github = Github(owner=OWNER, repository=REPO, api_token=GITHUB_TOKEN)
+github = Github(owner=OWNER, repository=REPO, sleep_for_rate=True, api_token=[GITHUB_TOKEN])#, api_token=GITHUB_TOKEN)
+
+# Fetch pull requests
+pr_data = []
+count = 0
+max_prs = 2000  # Limit to the first 2000 PRs
+
+print(f"Fetching pull requests for {OWNER}/{REPO}...")
+for pr in github.fetch(category="pull_request"):
+    # print(pr)
+    if pr['data']['state'] == 'closed':# and not pr['data']['merged']: #only rejected PRs
+        pr_data.append(pr)
+        count += 1
+        if count >= max_prs:
+            break
+    if count % 10 == 0:  # Status update every 100 PRs
+        print(f"Fetched {count} PRs, has data on {len(pr_data)}...")
+
+# Save data to a JSON file
+output_file = "signal_android_prs.json"
+with open(output_file, "w") as f:
+    json.dump(pr_data, f, indent=4)
+
+print(f"Saved {len(pr_data)} pull requests to {output_file}.")
